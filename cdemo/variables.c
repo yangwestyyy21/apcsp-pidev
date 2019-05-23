121449a77ad423a815ef773d062371ebd4c00d8a
weston yang <westonyang.777@gmail.com>
2019-05-23 08:59:07 -0700

    variable thing

---
+++ cdemo/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+  a = 5;
+  c = 2;
+  printf("a = %d, b = %d, c = %d\n", a, b, c);
+}
